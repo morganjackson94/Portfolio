60b9e8fd2a06e0a4b31f4f1607a328710929d892
Morgan Jackson <contactmojack@gmail.com>
2015-11-19 18:52:27 -0600

    11/19/15 6:47pm

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 (function() {
     
     $.fatNav();
-    $.stellar();
+    $.stellar();	
 
-	
+	function blink(selector){
+		$(selector).fadeOut(1000, function(){
+		    $(this).fadeIn(1000, function(){
+		        blink(this);
+		    });
+		});
+		}
+		    
+	blink('.scroll');
     
 }());
